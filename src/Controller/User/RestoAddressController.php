39ef31b9f8dff73991a61b132062bd7738f6fcc7
SebastienJEANDEL <seb54.jeandel@gmail.com>
2022-07-19 16:36:17 +0200

    wip good address

---
+++ src/Controller/User/RestoAddressController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Entity\Establishment;
+use App\Entity\Tag;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\EstablishmentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RestoAddressController extends AbstractController
+{
+    /**
+     * @Route("/bonnes-adresses", name="app_user_resto_address")
+     */
+    public function listResto(EstablishmentRepository $establishmentRepository, ?Style $style, ?Tag $tag): Response
+    {
+        if ($style){
+           
+        }elseif ($tag){
+
+        }
+        $listEstablishment = $establishmentRepository->findAll();
+        
+        return $this->render('user/resto_address/index.html.twig', ['listEstablishment'=>$listEstablishment]);
+    }
+}
