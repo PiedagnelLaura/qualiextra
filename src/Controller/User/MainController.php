4feebd09884177176eeea69cdb2f1c843a18f0de
Vanessa-Peixoto <vanessa.peixoto.dev@gmail.com>
2022-07-19 16:21:13 +0200

    route create + show package list in twig

---
+++ src/Controller/User/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Repository\PackageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home", methods={"GET"})
+     */
+    public function home(PackageRepository $packageRepository): Response
+    {
+        $packageList = $packageRepository->findAll();
+
+
+        return $this->render(
+            'User/main/index.html.twig', 
+            ['packageList' => $packageList]
+        );
+    }
+}
