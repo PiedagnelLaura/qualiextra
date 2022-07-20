822f1e4cb361ad3d310077e2780c56a5b7ab7ea0
Vanessa-Peixoto <vanessa.peixoto.dev@gmail.com>
2022-07-20 13:39:49 +0200

    nav filter type

--- src/Controller/User/MainController.php
+++ src/Controller/User/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\User;
 
 use App\Repository\PackageRepository;
+use App\Repository\TypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,14 +13,18 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home", methods={"GET"})
      */
-    public function home(PackageRepository $packageRepository): Response
+    public function home(PackageRepository $packageRepository, TypeRepository $typeRepository): Response
     {
         $packageList = $packageRepository->findAll();
+        $typeList = $typeRepository->findAll();
 
 
         return $this->render(
             'User/main/home.html.twig', 
-            ['packageList' => $packageList]
+            [
+                'packageList' => $packageList,
+                'typeList' => $typeList,
+            ]
         );
     }
 }
