a1d62fd76cb4fe12a950fd54898405dbee3dda82
PiedagnelLaura <piedagnel.laura@gmail.com>
2022-07-21 17:21:28 +0200

    markers of the map are added/removed based on the filters

--- public/asset/js/coordinate.js
+++ public/asset/js/coordinate.js
@@ -1,31 +1,52 @@
 const coordinate = {
+    // in this table, we will put the list of markers
+    markerList : [],
+
     init: function () {
-        console.log('coucou');
+        // we put a listener on the checkboxes filters
+        const filtersCheckboxList = document.querySelectorAll('.checkbox-filters');
+        for (const filterCheckbox of filtersCheckboxList) {
+            filterCheckbox.addEventListener('change', coordinate.handleChangeMarker);
+        }
+
+        coordinate.markerShow(); 
+    },
+
+    /**
+     * allows to get the list of establishments and to place all markers on the map
+     */
+    markerShow: function () {
+
         const listEstablisment = document.querySelectorAll('.establishment');
-   
+
         for (let establishment of listEstablisment) {
 
+            if (!establishment.classList.contains('d-none')) {
 
-            let latEstablishment = establishment.dataset.lat;
-            let longEstablishment = establishment.dataset.long;
+                let latEstablishment = establishment.dataset.lat;
+                let longEstablishment = establishment.dataset.long;
+                
+                // for each establishment, we add a marker on the map
+                let marker = L.marker([latEstablishment, longEstablishment]).addTo(map);
+    
+                // we add a popup when we click on the marker
+                marker.bindPopup('');
 
-            // for each establishment, we add a marker on the map
-            var marker = L.marker([latEstablishment, longEstablishment]).addTo(map);
-
-            // we add a popup when we click on the marker
-            marker.bindPopup('');
-            console.log(marker);
-
-           
+                coordinate.markerList.push(marker); 
+            }
         }
-      
-
-        map.on('click', function () {
-            map.removeLayer(marker);
-          });
-
 
     },
 
-    
+    /**
+     * if we add a filter, we delete all markers before adding others
+     */
+    handleChangeMarker: function () {
+
+        for (markerOne of coordinate.markerList) {
+            map.removeLayer(markerOne);
+        }
+
+        coordinate.markerShow();   
+    }
 }
