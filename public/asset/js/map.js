e4bbc1414167ec68cc33a539b12602f488b29de2
PiedagnelLaura <piedagnel.laura@gmail.com>
2022-07-21 19:06:50 +0200

    change the location of js files

--- public/asset/js/map.js
+++ public/asset/js/map.js
@@ -1,6 +1,6 @@
 
 // we add the map and center it on Paris
-const map = L.map('map').setView([48.8588897,2.320041], 12);
+const map = L.map('map-establishment').setView([48.8588897,2.320041], 12);
 L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
