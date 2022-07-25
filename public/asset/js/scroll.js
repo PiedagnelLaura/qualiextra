4c034080a40b2feea8fde99c773505aa857b46cf
Vanessa-Peixoto <vanessa.peixoto.dev@gmail.com>
2022-07-25 09:46:18 +0200

    add comments

--- public/asset/js/scroll.js
+++ public/asset/js/scroll.js
@@ -1,3 +1,5 @@
+//Scroll function 
+
 function isScrolledIntoView(el) {
     const rect = el.getBoundingClientRect();
     const elemTop = rect.top;
@@ -5,7 +7,7 @@ function isScrolledIntoView(el) {
     const isVisible = elemTop < window.innerHeight && elemBottom >= 0;
 
     if (isVisible) {
-        console.log('visible!!')
+        
         if (window[el.id] != true) {
             el.querySelector(".fixed-title").classList.add('active');
 
@@ -19,7 +21,7 @@ function isScrolledIntoView(el) {
 
 const packageTypes = ['experience-hoteliere', 'degustation-de-spiritueux', 'diner', 'experience-atypique'];
 
-//document.addEventListener('scroll', isScrolledIntoView.bind(null, document.getElementById('degustation-de-spiritueux')));
+
 for(let i = 0; i < packageTypes.length; i++) {
     document.addEventListener('scroll', isScrolledIntoView.bind(null, document.getElementById(packageTypes[i])));
 }
