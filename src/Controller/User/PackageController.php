88d029462e06b8795b9659ec896d6b3bc5192b9d
AurelieDombre <au.dombre@gmail.com>
2022-07-25 16:21:17 +0200

    bug to push in database

--- src/Controller/User/PackageController.php
+++ src/Controller/User/PackageController.php
@@ -36,118 +36,86 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 class PackageController extends AbstractController
 {
 
-    private $sessionTab;
-    public function __construct(SessionInterface $session, UserRepository $userRepository)
-    {
-        $this->sessionTab = $session->get('user') ?? [];
-        $this->userRepository = $userRepository;
-    }
 
     /**
      * Show one package by id
      *
-     * @Route("/packages/{id}", name="app_user_package_show", requirements={"id"="\d+"}, methods={"GET"})
+     * @Route("/packages/{id}", name="app_user_package_show", requirements={"id"="\d+"}, methods={"GET", "POST"})
      *
      *  @param [type] $id
      */
-    public function packageShow($id, ManagerRegistry $doctrine)
+    public function packageShow($id, ManagerRegistry $doctrine,Request $request, BookRepository $bookRepository)
     {
+        
         // Alternative for access Repository of entity Package we manage with ManagerRegistry for grib the repository
         $PackageRepository = $doctrine->getRepository(Package::class);
 
-        
-
         // show package by id
         $package = $PackageRepository->find($id);
-        
+
+        // dd($package);
         // Package not found ?
         if ($package === null) {
             throw $this->createNotFoundException('Package don\'t find');
         }
         
-        return $this->render('User/packageShow.html.twig', [
-            'package' => $package,
-        ]);
-    }
-
-    /**
-     * Méthode qui permet d'ajouter une réservation 
-     * 
-     * @Route("/packages/{id}", name="app_package-book", requirements={"id"="\d+"}, methods={"POST"})
-     */
-    public function book(int $id,
-                        ManagerRegistry $doctrine,
-                        Request $request, 
-                        Package $package, 
-                        MailerService $mailerService): JsonResponse
-    {
-        //TODO : Après avoir fait le formulaire d'inscription
-        //Récupérer l'utilisateur connecté
-        $userRepository = $doctrine->getRepository(User::class);
-        
-
         //instanciation de book
         $newBook = new Book();
-
         $user = new User();
 
         $form = $this->createForm(BookType::class, $newBook);
         $form->handleRequest($request);
-        
+
+        //dd('avant le if');
         
         if ($form->isSubmitted() && $form->isValid()) {
 
+            dd('dans le if');
             //Ajout user rattaché : TODO authentification
             $newBook->setUser($user);
 
             //Ajout du package associé à la réservation
             $newBook->setPackages($package);
+            
             //Info par défault, status et prix (obligatoire pour valider l'ajout)
             $newBook->setStatus(0);
             $newBook->setPrice($package->getPrice());
-            
-            dd("je suis là");
             $form->add($newBook, true);
-            dd("je suis là ici");
-            // Instant où on enregistre tout en BDD :
 
-            // $entityManager = $doctrine->getManager();
-            // $entityManager->persist($newBook);
-            
-            // $entityManager->flush();
+            dd($newBook);
 
-            // $data = $form->getData();
-            
+            //Envoi de mail au click du bouton réservation
+            // $mailerService->send(
+            //     "nouvelle réservation",
+            //     "client@exemplemail.com",
+            //     "contact@testqualiextra.com",
+            //     "emails/email.html.twig'", 
+            //     [
+            //         "Nom" => $user["lastname"],
+            //         "E-mail" => $user["email"],
+            //         "Prix" => $package["price"],
+            //         "Date de réservation" => $newBook["date"],
+            //     ]
+            //     );
 
-            $mailerService->send(
-                "nouvelle réservation",
-                "client@exemplemail.com",
-                "contact@testqualiextra.com",
-                "emails/email.html.twig'", 
-                [
-                    "Nom" => $user["lastname"],
-                    "E-mail" => $user["email"],
-                    "Prix" => $package["price"],
-                    "Date de réservation" => $newBook["date"],
-                ]
-                );
+            //Flash Message pour le client
+            $this->addFlash('success-book', 'Votre réservation est en cours de confirmation.');
 
             
             return $this->redirectToRoute('app_user_home', [], Response::HTTP_SEE_OTHER);
         }
-        dd("je suis en dehors du if");
-        //Flash Message pour le client
-        $this->addFlash('success-book', 'Votre réservation est en cours de confirmation.');
 
-        //envoi au format JSON info de la réservation
-        // return $this->json(['book' => json_decode($serializer->serialize($newBook, 'json', ['groups' => ['normal']]))]);
-        dd($form);
-        // redirection vers la page 
-        // return $this->redirectToRoute('app_user_home', [], Response::HTTP_SEE_OTHER);
-        return $this->renderForm('User/packageShow.html.twig', ["form" => $form]);
-        dd($form);
+        //! Formulaire booking
+
+        
+        return $this->renderForm('User/packageShow.html.twig', [
+            'package' => $package,
+            'form' => $form
+        ]);
+
     }
 
+    
 
         /**
      * Show email
