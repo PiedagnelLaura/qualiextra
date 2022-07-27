bdefac2690ad4c420c008f3e44cffca124889e9b
PiedagnelLaura <piedagnel.laura@gmail.com>
2022-07-27 11:57:16 +0200

    bookForm ok

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Book;
-use DateTimeInterface;
+
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,11 +18,13 @@ class BookType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date', DateTimeInterface::class)
+            ->add('date', DateType::class, [
+                'label' => ' ',
+                'widget' => 'single_text',])
 
-            ->add('name', TextType::class)
-            ->add('price', IntegerType::class )
-            ->add('email', EntityType::class, ['class' => User::class])
+            //->add('name', TextType::class)
+            //->add('price', IntegerType::class )
+            //->add('email', EntityType::class, ['class' => User::class])
         ;
     }
 
