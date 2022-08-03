b0169f2a80c31afbc9f7d131430a82d03fc8b21b
PiedagnelLaura <piedagnel.laura@gmail.com>
2022-08-03 10:08:35 +0200

    test formEdit

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,7 @@ class RegisterType extends AbstractType
     {
         $builder
             ->add('email',  EmailType::class)
-            ->add('password', TextType::class, [
+            ->add('password', PasswordType::class, [
                 'label' => 'Mot de passe'])
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom'])
